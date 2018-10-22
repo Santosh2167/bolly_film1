0d7ac87c303f093d301af19b6546f50929cf2544
Santosh <santosh.poudyal@gmail.com>
2018-10-22 19:21:41 +1100

    added edit, delete and update buttons

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,23 @@
 class MoviesController < ApplicationController
   def index
+    @movie33 = Movie.all
   end
 
   def show
+    @movie1 = Movie.find(params[:id])
   end
 
   def create
-    render json => params
+    # render json: params[:movie]
+    @movie = Movie.new(save_data)
+    @movie.save
+
+    redirect_to @movie
+  end
+
+  private def save_data
+    params.require(:movie).permit(:title,:runtime,:genre,:release_date)
+
   end
 
   def new
@@ -14,11 +25,18 @@ class MoviesController < ApplicationController
   end
 
   def update
+    @movie = Movie.find(params[:id])
+    @movie.update(save_data)
+
+    redirect_to @movie
   end
 
   def edit
+    @movie = Movie.find(params[:id])
+    # render json: @movie
   end
 
   def destroy
+    render json: params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
 
   get "movies", to: "movies#index"
   post "movies", to: "movies#create"
-  get "movies/new", to: "movies#new"
-  get "movies/:id/edit", to: "movies#edit"
-  get "movies/:id",to: "movies#show"
+  get "movies/new", to: "movies#new", as: 'new_movie'
+  get "movies/:id/edit", to: "movies#edit", as: "edit_movie"
+  get "movies/:id",to: "movies#show", as: 'movie'
   put "movies/:id",to: "movies#update"
   patch "movies/:id",to: "movies#update"
   delete "movies/:id",to: "movies#destroy"
