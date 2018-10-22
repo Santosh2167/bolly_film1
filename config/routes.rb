b3c565019cb0b285e835722dbe082c29a00246c0
Santosh <santosh.poudyal@gmail.com>
2018-10-22 17:13:36 +1100

    created form in the view, updated method in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # get 'movies/index'
+  # get 'movies/show'
+  # post 'movies/create'
+  # get 'movies/new'
+  # put 'movies/update'
+  # patch 'movies/update'
+  # get 'movies/edit'
+  # delete 'movies/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "movies", to: "movies#index"
+  post "movies", to: "movies#create"
+  get "movies/new", to: "movies#new"
+  get "movies/:id/edit", to: "movies#edit"
+  get "movies/:id",to: "movies#show"
+  put "movies/:id",to: "movies#update"
+  patch "movies/:id",to: "movies#update"
+  delete "movies/:id",to: "movies#destroy"
+
+
 end
